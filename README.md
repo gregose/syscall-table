4e00d3e1a6b32689d56aab14b3e9797eda4f0d30
Paolo Stivanin <info@paolostivanin.com>
2014-06-25 08:49:34 +0200

    updated to kernel version 3.15.1

--- README.md
+++ README.md
@@ -6,9 +6,10 @@ Generate JSON system call table from Linux source. Hosted at http://syscalls.ker
 * Install ctags (http://ctags.sourceforge.net)
 * `easy_install python-ctags simplejson`
 * Download and extract the Linux kernel sources (https://www.kernel.org)
+* Move the extraced folder to `/usr/src/linux-$VERSION`
 * `chmod +x prepare-files.sh && ./prepare-files.sh`
-<br>:coffee: or :beer:
-* `chmod +x gen_syscalls.py && ./gen_syscalls.py > www/syscalls-x86.json`
+* :coffee: or :beer:
+* `chmod +x gen_syscalls.py && ./gen_syscalls.py > www/syscalls-x86.js`
 
 ## Web
 * uses [jQuery DataTables](http://datatables.net/) to pull JSON file and format table
--- prepare-files.sh
+++ prepare-files.sh
@@ -1,20 +1,21 @@
 #!/bin/sh
 
-if [ ! -d /usr/src/linux ];then
+KERNEL_VERSION="3.15.1"
+
+if [ ! -d /usr/src/linux-${KERNEL_VERSION} ];then
 	echo "The Linux source tree doesn't exist"
 	exit -1
 fi
 
-if [ ! -f /usr/src/linux/arch/x86/syscalls/syscall_32.tbl ];then
+if [ ! -f /usr/src/linux-${KERNEL_VERSION}/arch/x86/syscalls/syscall_32.tbl ];then
 	echo "File syscall_32.tbl doesn't exist"
 	exit -1
 fi
 
 echo "[+] Generating tags, this may take a while..."
-ctags --fields=afmikKlnsStz --c-kinds=+pc -R /usr/src/linux
-cp -v tags .
+ctags --fields=afmikKlnsStz --c-kinds=+pc -R /usr/src/linux-${KERNEL_VERSION}
 echo "[+] Tags generated"
 echo "[+] Preparing the syscall table file..."
-cp -v /usr/src/linux/arch/x86/syscalls/syscall_32.tbl .
+cp -v /usr/src/linux-${KERNEL_VERSION}/arch/x86/syscalls/syscall_32.tbl .
 sed -i '1,8d' syscall_32.tbl
 echo "[+] Done :)"
